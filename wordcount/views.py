56ee9cdf99ca3f271ff7e1edbe648ab4910ccbe5
George Lathrop <airtalky@gmail.com>
2020-12-22 23:03:46 -0500

    This is my first comit

---
+++ wordcount/views.py
@@ -0,0 +1,42 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+import operator
+
+def home(request):
+    #return HttpResponse('Hello')
+    #return render(request, 'home.html', {'hithere':'This is me'})
+    return render(request, 'home.html')
+
+"""
+def eggs(request):
+    #return HttpResponse('Eggs are great!')
+    return HttpResponse('<h1>EGGS</h1>')
+"""
+
+def count(request):
+    fulltext = request.GET['fulltext']
+    
+    #print(fulltext)
+    wordlist = fulltext.split()
+    
+    worddictionary = {}
+    
+    for word in wordlist:
+        if word in worddictionary:
+            #Increase
+            worddictionary[word] += 1
+        else:
+            #Add to the dictionary
+            worddictionary[word] = 1
+            
+    sortedwords = sorted(worddictionary.items(), key=operator.itemgetter(1), reverse=True)
+        
+    return render(request, 'count.html',{'fulltext':fulltext,'count':len(wordlist),'sortedwords':sortedwords})
+    
+def about(request):
+    abouttext='This is a website that counts words'
+    return render(request, 'about.html',{'abouttext':abouttext})
+    
+
+
+    
